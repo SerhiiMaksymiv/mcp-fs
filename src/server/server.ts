d488c107e01c2503c7af29df992457df6fb1f4d8
mcsymiv <SerhiiMaksymivQAMadness@brandgility.com>
2025-10-31 22:01:16 +0200

    export server module

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,6 @@
-#!/usr/bin/env node
-
-import { Server } from "@modelcontextprotocol/sdk/server/index.js"
+import { Server, ServerOptions } from "@modelcontextprotocol/sdk/server/index.js"
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
-import { CallToolRequestSchema, ListToolsRequestSchema } from "@modelcontextprotocol/sdk/types.js"
+import { CallToolRequestSchema, Implementation } from "@modelcontextprotocol/sdk/types.js"
 
 import { readFile } from "./tools/read.file.js"
 import { readMultipleFiles } from "./tools/read.multiple.files.js"
@@ -17,66 +15,75 @@ import { fileInformation } from "./tools/file.information.js"
 import { allowedDirectories } from "./paths.js"
 import { tools } from "./tools.js"
 
-const server = new Server(
-  {
-    name: "secure-filesystem-server",
-    version: "0.2.0",
-  },
-  {
-    capabilities: {
-      tools: {},
-    },
-  },
-)
+class McpServer {
+  private server: Server
 
-server.setRequestHandler(ListToolsRequestSchema, async () => ({ ...tools }))
+  constructor(
+    _serverInfo: Implementation = { name: "secure-filesystem-server", version: "0.2.0" },
+    options: ServerOptions = { capabilities: { tools: tools } }
+  ) {
 
-server.setRequestHandler(CallToolRequestSchema, async (request) => {
-  try {
-    const { name, arguments: args } = request.params
+    this.server = new Server(_serverInfo, { ...options })
 
-    switch (name) {
-      case "read_file": { return readFile(args) }
-      case "read_multiple_files": { return readMultipleFiles(args) }
-      case "write_file": { return writeFile(args) }
-      case "edit_file": { return editFile(args) }
-      case "create_directory": { return createDirectory(args) }
-      case "list_directory": { return listDirectory(args) }
-      case "move_file": { return moveFile(args) }
-      case "search_files": { return searchFile(args) }
-      case "get_file_info": { return fileInformation(args) }
-      case "tree": { return directoryTree(args) }
+    this.server.setRequestHandler(CallToolRequestSchema, async (request) => {
+      try {
+        const { name, arguments: args } = request.params
 
-      case "list_allowed_directories": {
+        switch (name) {
+          case "read_file": { return readFile(args) }
+          case "read_multiple_files": { return readMultipleFiles(args) }
+          case "write_file": { return writeFile(args) }
+          case "edit_file": { return editFile(args) }
+          case "create_directory": { return createDirectory(args) }
+          case "list_directory": { return listDirectory(args) }
+          case "move_file": { return moveFile(args) }
+          case "search_files": { return searchFile(args) }
+          case "get_file_info": { return fileInformation(args) }
+          case "tree": { return directoryTree(args) }
+
+          case "list_allowed_directories": {
+            return {
+              content: [{
+                type: "text",
+                text: `Allowed directories:\n${allowedDirectories.join('\n')}`
+              }],
+            }
+          }
+
+          default:
+            throw new Error(`Unknown tool: ${name}`)
+        }
+      } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : String(error)
         return {
-          content: [{
-            type: "text",
-            text: `Allowed directories:\n${allowedDirectories.join('\n')}`
-          }],
+          content: [{ type: "text", text: `Error: ${errorMessage}` }],
+          isError: true,
         }
       }
+    })
+  }
 
-      default:
-        throw new Error(`Unknown tool: ${name}`)
-    }
-  } catch (error) {
-    const errorMessage = error instanceof Error ? error.message : String(error)
-    return {
-      content: [{ type: "text", text: `Error: ${errorMessage}` }],
-      isError: true,
+  // import { Transport } from "@modelcontextprotocol/sdk/shared/transport.js"
+  // connect method can be extended to support different transports, by passing a transport interface 
+  async connect() {
+    try {
+      await this.server.connect(new StdioServerTransport())
+      console.log("Secure MCP Filesystem Server running on stdio")
+      console.log("Allowed directories:", allowedDirectories)
+    } catch (error) {
+      console.error("More fine-grained error connecting to server:", error)
+      process.exit(1)
     }
   }
-})
-
-// Start server
-async function runServer() {
-  const transport = new StdioServerTransport()
-  await server.connect(transport)
-  console.error("Secure MCP Filesystem Server running on stdio")
-  console.error("Allowed directories:", allowedDirectories)
 }
 
-runServer().catch((error) => {
-  console.error("Fatal error running server:", error)
+async function main() {
+  new McpServer().connect()
+}
+
+main().catch((error) => {
+  console.error("Error running server:", error)
   process.exit(1)
 })
+
+export { McpServer }
